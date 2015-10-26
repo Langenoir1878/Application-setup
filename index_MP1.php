574a573c595067551e109450c7b16beda48241b9
langenoir <Ln1878@dhcp226.merusouth115.iit.edu>
2015-10-26 08:40:50 -0500

    modified layout

--- index_MP1.php
+++ index_MP1.php
@@ -46,9 +46,12 @@ session_start(); ?>
     <!-- MAX_FILE_SIZE must precede the file input field -->
     <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
     <!-- Name of input element determines name in $_FILES array -->
-    Send this file: <input name="userfile" type="file" /><br />
-Enter Email of user: <input type="email" name="useremail"><br />
-Enter Phone of user (1-XXX-XXX-XXXX): <input type="phone" name="phone">
+    <br><font color = "#00FF00">
+    Send this file: </font><input name="userfile" type="file" /><br />
+    <br><font color = "#00FF00">
+Enter Email of user: </font><input type="email" name="useremail"><br />
+	<br><font color = "#00FF00">
+Enter Phone of user (1-XXX-XXX-XXXX): </font><input type="phone" name="phone">
 
 
 <input type="submit" value="Send File" />
@@ -56,8 +59,9 @@ Enter Phone of user (1-XXX-XXX-XXXX): <input type="phone" name="phone">
 <hr />
 <!-- The data encoding type, enctype, MUST be specified as below -->
 <form enctype="multipart/form-data" action="gallery.php" method="POST">
-    
-Enter Email of user for gallery to browse: <input type="email" name="email">
+  
+  <br><font color = "#00FF00">  
+Enter Email of user for gallery to browse: </font><input type="email" name="email">
 <input type="submit" value="Load Gallery" />
 </form>
 
