e3ac5f6ccf7c97702d5d68bc81315c93b48d8339
langenoir <Ln1878@dhcp46.merusouth117.iit.edu>
2015-11-04 13:28:21 -0600

    updated password and user name for db connection

--- setup.php
+++ setup.php
@@ -10,22 +10,24 @@
 
 
 // Start the session^M
-require 'vendor/autoload.php';
-$rds = new Aws\Rds\RdsClient([
-    'version' => 'latest',
-    'region'  => 'us-east-1'
-]);
+//require 'vendor/autoload.php';
+//$rds = new Aws\Rds\RdsClient([
+  //  'version' => 'latest',
+    //'region'  => 'us-east-1'
+//]);
 $result = $rds->createDBInstance([
-    'AllocatedStorage' => 10,
+  //  'AllocatedStorage' => 10,
     #'AutoMinorVersionUpgrade' => true || false,
     #'AvailabilityZone' => '<string>',
     #'BackupRetentionPeriod' => <integer>,
    # 'CharacterSetName' => '<string>',
    # 'CopyTagsToSnapshot' => true || false,
    # 'DBClusterIdentifier' => '<string>',
-    'DBInstanceClass' => 'db.t1.micro', // REQUIRED
-    'DBInstanceIdentifier' => 'mp1-jrh', // REQUIRED
-    'DBName' => 'customerrecords',
+    //'DBInstanceClass' => 'db.t1.micro', // REQUIRED
+    //'DBInstanceIdentifier' => 'mp1-jrh', // REQUIRED
+  'DBInstanceClass' => 'db.t1.micro', // REQUIRED
+  'DBInstanceIdentifier' => 'SIMMON-THE-CAT-DB', // REQUIRED
+    //'DBName' => 'customerrecords',
     #'DBParameterGroupName' => '<string>',
     #'DBSecurityGroups' => ['<string>', ...],
     #'DBSubnetGroupName' => '<string>',
@@ -34,8 +36,10 @@ $result = $rds->createDBInstance([
     #'Iops' => <integer>,
     #'KmsKeyId' => '<string>',
    # 'LicenseModel' => '<string>',
-  'MasterUserPassword' => 'letmein888',
-    'MasterUsername' => 'controller',
+  //'MasterUserPassword' => 'letmein888',
+    //'MasterUsername' => 'controller',
+    'MasterUserPassword' => 'hesaysmeow',
+    'MasterUsername' => 'LN1878',
     #'MultiAZ' => true || false,
     #'OptionGroupName' => '<string>',
     #'Port' => <integer>,
@@ -57,15 +61,17 @@ $result = $rds->createDBInstance([
 ]);
 print "Create RDS DB results: \n";
 # print_r($rds);
-$result = $rds->waitUntil('DBInstanceAvailable',['DBInstanceIdentifier' => 'mp1-jrh',
-]);
+$result = $rds->waitUntil('DBInstanceAvailable',['DBInstanceIdentifier' => 'SIMMON-THE-CAT-DB']);
 // Create a table 
 $result = $rds->describeDBInstances([
-    'DBInstanceIdentifier' => 'mp1-jrh',
+    'DBInstanceIdentifier' => 'SIMMON-THE-CAT-DB'
 ]);
+
 $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
 print "============\n". $endpoint . "================\n";
-$link = mysqli_connect($endpoint,"controller","letmein888","3306") or die("Error " . mysqli_error($link)); 
+
+
+$link = mysqli_connect($endpoint,"LN1878","hesaysmeow","3306") or die("Error " . mysqli_error($link)); 
 echo "Here is the result: " . $link;
 $sql = "CREATE TABLE comments 
 (
@@ -74,5 +80,6 @@ PosterName VARCHAR(32),
 Title VARCHAR(32),
 Content VARCHAR(500)
 )";
+
 $con->query($sql);
 ?>
