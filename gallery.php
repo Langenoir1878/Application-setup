b14568e1fa167a65c498ad6e115d4e73a0370e72
Langenoir1878 <yzhan214@hawk.iit.edu>
2015-11-05 10:15:38 -0600

    Update gallery.php
    added database connections

--- gallery.php
+++ gallery.php
@@ -65,6 +65,56 @@
         </div>
         <!-- /.container -->
     </nav>
+ 
+     <!--DB connection-->
+    <?php
+
+    session_start();
+
+    $email = $_POST["email"];
+
+    echo $email;
+
+    require 'vendor/autoload.php';
+
+
+
+    use Aws\Rds\RdsClient;
+
+    $client = RdsClient::factory(array(
+    'version' =>'latest',
+    'region'  => 'us-west-2'
+    ));
+    
+    $link = mysqli_connect("itmo544-ght-db.c4bvwzwqz1hy.us-west-2.rds.amazonaws.com","guhaotian","909690ght","guhaotiandb") or die("Error " . mysqli_error($link));
+    /* check connection */
+    if (mysqli_connect_errno()) {
+        printf("Connect failed: %s\n", mysqli_connect_error());
+        exit();
+    }
+
+    //below line is unsafe - $email is not checked for SQL injection -- don't do this in real life or use an ORM instead
+
+    $link->real_query("SELECT * FROM items WHERE email = '$email'");
+
+    //$link->real_query("SELECT * FROM items");
+
+    $res = $link->use_result();
+
+    echo "Result set order...\n";
+
+    while ($row = $res->fetch_assoc()) {
+
+        echo "<img src =\" " . $row['s3rawurl'] . "\" /><img src =\"" .$row['s3finishedurl'] . "\"/>";
+
+    echo $row['id'] . "Email: " . $row['email'];
+
+    }
+
+    $link->close();
+
+    ?>
+
 
     <!-- Page Content -->
     <div class="container">
