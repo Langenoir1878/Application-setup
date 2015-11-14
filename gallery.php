558b8a0f38b4c188d4e74d605b25a0ad364958b7
langenoir <Ln1878@langenoirs-MacBook-Pro.local>
2015-11-13 20:27:05 -0600

    added back db connection block, TODO: relocate img into CSS containers

--- gallery.php
+++ gallery.php
@@ -69,9 +69,52 @@ session_start();
     </nav>
 
      <!--DB connection-->
-   <?php 
+    <?php 
 
-   echo "Hello";
+    $email = $POST["email"]
+    require 'vendor/autoload.php';
+
+    use Aws\Rds\RdsClient;
+    # updated Nov 13 for simple testing basic outcomes
+    $client = RdsClient::factory(array
+    'version' => 'latest',
+    'region'  => 'us-east-1'
+
+    ));
+
+    $result = $client->describeDBInstances(array(
+        'DBInstanceIdentifier' => 'SIMMION-THE-CAT-DB'
+    ));
+
+    $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
+
+    $link = mysqli_connect($endpoint,"LN1878","hesaysmeow","simmoncatdb") or die("Error " . mysqli_error($link));
+    //check connection
+    if (mysqli_connect_errno()) {
+        printf("Connect failed: %s\n", mysqli_connect_error());
+        exit();
+    }
+
+    //to be add =======================SELECT STATEMENT TO DISPLAY IMGS===============================================
+    //retrieving data from db, table name: CAT_TABLE
+    //NOTICE DIFFERENT FIELD NAMES: send - useremail - COLUMN "EMAIL"; retrieve - email - same column in db
+
+    $link->real_query("SELECT * FROM CAT_TABLE WHERE EMAIL = '$email'");
+
+    $res = $link->use_result();
+    //echo "Result set order...\n";
+    while ($row = $res->fetch_assoc()) {
+        #adding effects here
+    $urlINFO = "<img src =\" " . $row['RAWS3URL'] . "\" /><img src =\"" .$row['FINISHEDS3URL'] . "\"/>";
+    //echo $urlINFO;
+    $imageSTR = $row['ID'] . "Email: " . $row['EMAIL']; //to be used into CSS containers
+    //echo $imageSTR;
+    }
+
+    $link->close();
+
+
+    ?>
 
 
 
